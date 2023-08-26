17881cd15446959221736d259961117b9c3c0b31
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2023-08-26 12:06:27 +0500

    26.08.2023
    12:06

--- main/models.py
+++ main/models.py
@@ -1,8 +1,55 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Rectory(models.Model):  # Rektor va Direktorlar
     """Rector and Directors of Institute"""
-    pass
+    name = models.CharField(max_length=255, verbose_name="F.I.SH")
+    image = models.ImageField(upload_to="./rector/", verbose_name="Rasm")
+    content = RichTextUploadingField(verbose_name="Haqida")  # Translation
+
+    positon = models.TextField(verbose_name="Lavozimi")  # Translation
+    academic_title = models.TextField(verbose_name="Unvoni")  # Translation
+    is_rector = models.BooleanField(default=False, verbose_name="Rektormi?")
+    reception = RichTextUploadingField(verbose_name="Qabul vaqti")  # Translation
+
+    number = models.CharField(max_length=50, verbose_name="Telefon nomer", null=True, blank=True)
+    address = models.TextField(verbose_name="Manzil", null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True)
+    instagram = models.URLField(verbose_name="Instagram", null=True, blank=True)
+    telegram = models.URLField(verbose_name="Telegram", null=True, blank=True)
+    facebook = models.URLField(verbose_name="Facebook", null=True, blank=True)
+    linkedin = models.URLField(verbose_name="Linkedin", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):  # Markaz va Bo'limlar
+    """"""
+    name = models.CharField(max_length=255, verbose_name="Markaz nomi")  # Translation
+    content = RichTextUploadingField(verbose_name="Markaz haqida", null=True, blank=True)  # Translation
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=250, verbose_name="F.I.SH.")
+    image = models.ImageField(upload_to="./markaz/", default="./markaz/avatar.jpg", verbose_name="Rasm")
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name="Markaz")
+
+    position = models.TextField(verbose_name="Lavozimi")  # Translation
+    academic_title = models.TextField(verbose_name="Unvoni", null=True, blank=True)  # Translation
+    is_chief = models.BooleanField(default=False, verbose_name="Boshliqmi:")
+
+    number = models.CharField(max_length=50, verbose_name="Telefon nomer", null=True, blank=True)
+    email = models.EmailField(max_length=50, verbose_name="Email", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
--- page/models.py
+++ page/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Partner(models.Model):
+    name = models.CharField(max_length=250, verbose_name="Name")  # Translation
+    image = models.ImageField(upload_to="./partner/", verbose_name="Rasm")
+    url = models.URLField(verbose_name="Link")
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):  # Kategoriya Pages
     """Category of Pages"""
     pass
